5121e231f8fa5f2fc3ffef2dce0c1f976b0db57e
Vasundhara <vasudha.c8@gmail.com>
2017-08-24 22:21:40 -0700

    Started numbers from 999 to 100 in descending order

--- src/P4_LargestPalindromeProduct.java
+++ src/P4_LargestPalindromeProduct.java
@@ -3,9 +3,9 @@ public class P4_LargestPalindromeProduct {
     public static void main(String args[]){
 
         int largest = 0;
-        for (int i = 100; i < 1000; i++){
-            for (int r = 100; r < 1000; r++){
-                String x = Integer.toString(i * r);
+        for (int i = 999; i>=100; i--){
+            for (int r =999; r >=100; r--){
+                String x = Integer.toString(i* r);
                 String y = new StringBuilder(x).reverse().toString();
                 int strX = Integer.parseInt((x));
                 if (x.equals(y)) {
