3011e4294617f5636927e34628b796c11b1f7700
vasundharachimmad <vasudha.c8@gmail.com>
2017-08-30 15:39:58 -0700

    sum of all the primes below two million

---
+++ src/P10_SummationOfPrimes.java
@@ -0,0 +1,19 @@
+public class P10_SummationOfPrimes {
+    public static boolean isPrime(long n) {
+        if (n < 2) return false;
+        else if (n == 2) return true;
+        for (int i = 2; i < Math.pow(n, 0.5) + 1; i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        long sum = 0;
+        for (int i = 0; i < 2000000; i++)
+            if (isPrime(i))
+                sum += i;
+
+        System.out.print(sum);
+    }
+}
